0959fe1cf487dda2f5f185dd1b8f238e6d7b2150
Jack Heaton <75635917+EpictetusZ1@users.noreply.github.com>
2024-04-13 15:24:51 -0400

    route error v2

--- src/app/api/ai/route.ts
+++ src/app/api/ai/route.ts
@@ -1,6 +1,7 @@
 // gpt-3.5-turbo-0125
 import OpenAI from "openai";
 import { NextApiRequest, NextApiResponse } from "next";
+import { NextRequest } from "next/server";
 
 const openai = new OpenAI({
     apiKey: process.env["OPENAI_API_KEY"], // This is the default and can be omitted
@@ -22,12 +23,14 @@ const openai = new OpenAI({
 //     return Response.json({ message: chatCompletion})
 // }
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: Request) {
     if (req.method === "POST") {
-        const { runsData, goalType } = req.body;
+        // const { runsData, goalType } = req.body
 
         // Here you might summarize the runs data
-        const summary = `The user's average pace over the last two months is ${runsData.averagePace} minutes per kilometer, and their average distance is ${runsData.averageDistance} kilometers. Goal type: ${goalType}.`
+        // const summary = `The user's average pace over the last two months is ${runsData.averagePace} minutes per kilometer, and their average distance is ${runsData.averageDistance} kilometers. Goal type: ${goalType}.`
+
+        const summary = ""
 
         try {
             const chatCompletion = await openai.chat.completions.create({
@@ -38,13 +41,14 @@ export async function GET(req: NextApiRequest, res: NextApiResponse) {
                 ]
             })
 
-            res.status(200).json({ advice: chatCompletion.choices[0].message.content })
+            // res.status(200).json({ advice: chatCompletion.choices[0].message.content })
+            return Response.json({ advice: chatCompletion.choices[0].message.content })
+
         } catch (error) {
             console.error("OpenAI API error:", error)
-            res.status(500).json({ error: "Failed to fetch advice from OpenAI API." })
+            return Response.json({ error: "Failed to fetch data" })
         }
     } else {
-        res.setHeader("Allow", ["POST"])
-        res.status(405).end(`Method ${req.method} Not Allowed`)
+        return Response.json({ error: "Invalid request method" })
     }
 }
